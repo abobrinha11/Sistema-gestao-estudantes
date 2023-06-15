09714020a85a92907ac849da35239617322fa512
abobrinha11 <yuriselestino11@gmail.com>
2023-06-15 18:11:17 -0300

    Create Estudante.cs

---
+++ sistema-gestao-estudantes/Estudante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace sistema_gestao_estudantes
+{
+    internal class Estudante
+    {
+        MEU_BD bancodedados = new MEU_BD();
+        
+        //funcao que inclui o estudante no banco de dados.
+        public bool inserirestudante(string nome, string sobrenome, DateTime nascimento, 
+        string telefone, string genero, string endereco, MemoryStream foto)
+        {
+            return false;
+        }
+    }
+}
